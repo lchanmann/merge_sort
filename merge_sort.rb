7b21e3f98510b852e05e062d9cb0ae23f786c450
Mann <chanmannlim@gmail.com>
2014-07-08 00:56:01 +0700

    rubyize

--- merge_sort.rb
+++ merge_sort.rb
@@ -9,18 +9,11 @@ end
 
 def merge left, right
   result = []
-  while left.length + right.length > 0 do
-    if left.length > 0 && right.length > 0
-      if left.first <= right.first
-        result << left.shift
-      else
-        result << right.shift
-      end
-    elsif left.length > 0
-      result << left.shift
-    elsif right.length > 0
-      result << right.shift
-    end
+  (left.length + right.length).times do
+    a, b = left.first, right.first
+    result << (a <= b ? left.shift : right.shift) if a && b
+    result << left.shift unless b
+    result << right.shift unless a
   end
   result
 end
